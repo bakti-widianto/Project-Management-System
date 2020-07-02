709b2a26ccc1c4f64c8e70ae9d25af0acf18f3e0
bakti <bakti.widianto@gmail.com>
2020-07-02 14:57:40 +0700

    install session

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session')
 const { Pool } = require('pg')
 
 const pool = new Pool({
@@ -14,7 +15,8 @@ const pool = new Pool({
 })
 
 var indexRouter = require('./routes/index')(pool);
-var usersRouter = require('./routes/users');
+var projectsRouter = require('./routes/projects')(pool);
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -27,17 +29,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+  secret: 'webpemula'
+}))
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// app.use('/users', usersRouter);
+app.use('/projects', projectsRouter)
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
