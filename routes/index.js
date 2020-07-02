ac6e475492552aaf0b100ba76b9e14749b8df49a
bakti <bakti.widianto@gmail.com>
2020-07-03 06:41:32 +0700

    view login modifed

--- routes/index.js
+++ routes/index.js
@@ -8,27 +8,33 @@ const saltRounds = 10;
 module.exports = (db) => {
 
   router.get('/', function (req, res, next) {
-    res.render('login');
+    res.render('login', { loginInfo: req.flash('loginInfo')});
   });
 
   router.post('/login', (req, res) => {
     db.query('SELECT * FROM users WHERE email = $1', [req.body.email], (err, data) => {
       // console.log(data.rows)
-      if (err) return res.status(500).json({
-        error: true,
-        message: err
-      })
-      if (data.rows.length == 0) return res.send('data tidak di temukan')
+      if (err) {
+        req.flash('loginInfo', 'Terjadi Kesalahan, coba lagi beberapa saat kemudian.')
+        return res.redirect('/')
+      }
+      if (data.rows.length == 0) {
+        req.flash('loginInfo', 'Email atau Password salah !')
+        return res.redirect('/')
+      }
+
+
 
       bcrypt.compare(req.body.password, data.rows[0].password, function (err, result) {
-        if (err) return res.status(500).json({
-          error: true,
-          message: err
-        })
+        if (data.rows.length == 0) {
+          req.flash('loginInfo', 'Terjadi Kesalahan, coba lagi beberapa saat kemudian.')
+          return res.redirect('/')
+        }
         if (result) {
           req.session.user = data.rows[0]
           res.redirect('/projects')
         } else {
+          req.flash('loginInfo', 'Email atau Password salah !')
           res.redirect('/')
         }
       })
@@ -37,7 +43,7 @@ module.exports = (db) => {
 
   router.get('/logout', (req, res) => {
     req.session.destroy(function (err) {
-      if(err) return res.send(err)
+      if (err) return res.send(err)
     })
     res.redirect('/')
   })
