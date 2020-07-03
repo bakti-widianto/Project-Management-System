727f54820bbac5ed84008fedb17bcbec54705a47
bakti <bakti.widianto@gmail.com>
2020-07-04 00:25:59 +0700

    modifed dasrboard

--- routes/projects.js
+++ routes/projects.js
@@ -1,19 +1,42 @@
 var express = require('express');
 var router = express.Router();
-
-const isLoggedIn = (req, res, next) =>{
-  if(req.session.user){
-    next();
-  }else{
-    res.redirect('/')
-  }
-}
+const check = require('../check/check');
 
 module.exports = (db) => {
-  router.get('/', isLoggedIn, function (req, res, next) {
+  router.get('/', check.isLoggedIn, function (req, res, next) {
     let user = req.session.user
     res.render('projects',{user});
   });
+
+  router.get('/:projectid/overview', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.get('/:projectid/activity', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.get('/:projectid/member', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.get('/:projectid/member', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.post('/:projectid/member', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.get('/:projectid/member/:id', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+  router.post('/:projectid/member/:id', check.isLoggedIn, function (req, res, next) {
+    
+  });
+
+
 
   return router;
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,59 @@
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+module.exports = (db) => {
+  // //REGISTER USERS
+  router.get('/', function (req, res, next) {
+    db.query('select * from users', (err, data) => {
+      if (err) return res.status(500).json({
+        error: true,
+        message: err
+      })
+      res.status(200).json({
+        error: false,
+        data: data.rows
+      })
+    })
+  });
 
-module.exports = router;
+  router.post('/', function (req, res, next) {
+    bcrypt.hash(req.body.password, saltRounds, function (err, hash) {
+      // Store hash in your password DB.
+      if (err) return res.status(500).json({
+        error: true,
+        message: err
+      })
+
+
+      let sql = 'INSERT INTO users(email, password, firstname, lastname, position, typejob, role) VALUES ($1, $2, $3, $4, $5, $6, $7)'
+      let values = [req.body.email, hash, req.body.firstname, req.body.lastname, req.body.position, req.body.typejob, req.body.role]
+      db.query(sql, values, (err) => {
+        console.log(values)
+        if (err) return res.status(500).json({
+          error: true,
+          message: err
+        })
+        res.status(201).json({
+          error: false,
+          message: 'ADD COMPLETE'
+        });
+      })
+    });
+  });
+
+  router.get('/:id', (req, res) => {
+    let id = req.params.id
+    let sql = `SELECT * FROM users WHERE userid = ${id}`
+    db.query(sql, (err, data) => {
+      if (err) return res.status(500).json({
+        error: err,
+        message: err
+      })
+      res.send(data.rows)
+    })
+  })
+
+  return router;
+}
